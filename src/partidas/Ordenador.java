36c0b7381f3f5eade20420e2b2a08345452131c3
guilhermecarv32 <91295938+guilhermecarv32@users.noreply.github.com>
2023-09-30 15:44:46 -0300

    Update Ordenador.java

--- src/partidas/Ordenador.java
+++ src/partidas/Ordenador.java
@@ -3,6 +3,12 @@ package partidas;
 import java.util.List;
 
 public abstract class Ordenador<T> {
+	
+	/*
+	classe que determina as funcionalidades do ordenador, 
+	cuja complexidade é constante, O(1)
+	*/
+	
     protected List<T> leituras = null;
 
     public Ordenador(List<T> leituras) {
