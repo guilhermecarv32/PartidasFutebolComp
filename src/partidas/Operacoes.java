e1e3ba1bb13ad3c9696618236ad6f05240703410
guilhermecarv32 <91295938+guilhermecarv32@users.noreply.github.com>
2023-09-30 15:44:18 -0300

    Update Operacoes.java

--- src/partidas/Operacoes.java
+++ src/partidas/Operacoes.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Map;
 
 public interface Operacoes<K, V> {
+	
+	//d1
     void imprimir(List<K> monitorados);
-
+    
+    //d2
     void imprimir(Map<K, List<V>> leituras);
 
+    //d3
     Map<K, List<V>> ordenar(Map<K, List<V>> leituras);
 
-    public double calcularMediaVelocidadesCubico(Map<Partida, List<Chute>> leituras);
+    //d4
+    public double calcularMediaVelocidades(Map<Partida, List<Chute>> leituras);
 }
