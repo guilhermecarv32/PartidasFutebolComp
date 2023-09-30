8c4ffbf0ab846b90f06a2abc04ef82c5e9ed476e
guilhermecarv32 <91295938+guilhermecarv32@users.noreply.github.com>
2023-09-30 15:45:18 -0300

    Update Partida.java

--- src/partidas/Partida.java
+++ src/partidas/Partida.java
@@ -1,6 +1,12 @@
 package partidas;
 
 public class Partida implements Comparable<Partida> {
+	
+	/*
+	classe para simular a coisa monitorada, uma partida de futebol,
+	cuja complexidade é constante, O(1)
+	*/
+	
     private String nome;
 
     public Partida(String nome) {
